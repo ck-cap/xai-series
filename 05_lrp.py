84439791b21777b8535f8697cb57d56e879738a0
ck-cap <23076218@siswa.um.edu.my>
2025-12-20 01:14:01 +0800

    update lrp

--- 05_lrp.py
+++ 05_lrp.py
@@ -45,7 +45,7 @@ print(f"Number of classes: {num_classes}")
 class CNNModel(nn.Module):
     def __init__(self, num_classes=4):
         super(CNNModel, self).__init__()
-        self.vgg16 = models.vgg16(pretrained=True) 
+        self.vgg16 = models.vgg16(weights=models.VGG16_Weights.IMAGENET1K_V1) 
 
         # Replace output layer according to our problem
         in_feats = self.vgg16.classifier[6].in_features 
